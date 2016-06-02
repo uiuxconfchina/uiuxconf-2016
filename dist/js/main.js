228f696697bc1818098530005e765ea68b41cec1
z4rd <z4rd@live.cn>
2016-06-03 02:47:58 +0800

    site update

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,15 +1,29 @@
-var navBtn = $('button.nav'),
-body = $('body');
+var body = $('body');
 
 function toggleMenu() {
-  navBtn.click(function() {
+  $('button.nav').click(function() {
     body.toggleClass('menu-open');
   })
-  $('button.close, a.about').click(function() {
+  $('#header button.close, a.about').click(function() {
     body.removeClass('menu-open');
+  })
+}
+
+function toggleModal() {
+  $('.cta').click(function(){
+  	body.toggleClass('modal-open');
+  })
+  $(this).click(function(e) {
+    var eTarget = $(e.target);
+    if ($(eTarget).hasClass('scroller') || eTarget.hasClass('modal')) {
+      body.toggleClass('modal-open');
+    } else {
+      e.stopPropagation();
+    }
   })
 }
 
 $(document).ready(function() {
   toggleMenu();
+  toggleModal();
 })
